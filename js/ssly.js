92c85f9424f8b53e0017e87ae19af2af51eed664
Yan825207 <2052509604@qq.com>
2020-05-06 21:48:50 +0800

    1

--- js/ssly.js
+++ js/ssly.js
@@ -41,6 +41,7 @@ $(function () {
         speed: 1000,
         direction: 'horizontal', // 垂直切换选项
         loop: true, // 循环模式选项
+        autoplay: true,
         slidesPerView: 4,
         slidesPerGroup: 1,
         spaceBetween: 0,
@@ -54,11 +55,9 @@ $(function () {
     $(".main5 .m4hover").hover(function () {
         $(this).addClass("m4hover_ing")
         for (var i = 0; i < $(".main5 div").length; i++) {
-            console.log($);
 
         }
     }, function () {
-
         $(this).removeClass("m4hover_ing")
     })
 })
